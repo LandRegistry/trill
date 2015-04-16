f802db2117411671dc4f31f79cf86193bfa51fab
alex blewett <alex.blewett@digital.landregistry.gov.uk>
2015-04-16 12:21:18 +0100

    front end

--- application/views.py
+++ application/views.py
@@ -1,5 +1,23 @@
 from application import app
+from flask import render_template
+#, flash, redirect, request, session, make_response
+
+'''@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/view')
+def view_skills():
+    return render_template('view_skills.html')
+
+@app.route('/edit')
+def edit_skills():
+    return render_template('edit_skills.html')
+
+@app.route('/admin')
+def admin_func():
+    return render_template('admin_func.html')'''
 
 @app.route('/')
-def check_status():
-    return "Everything is OK"
+def view_skills():
+    return render_template('test.html')
