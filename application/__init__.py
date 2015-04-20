ed4cbd69d0f873436235c230bc56711ddf8ed345
alex blewett <alex.blewett@digital.landregistry.gov.uk>
2015-04-20 10:55:02 +0100

    weird SQLAlchemy issue

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from flask.ext.sqlalchemy import SQLAlchemy
+
+#doesn't seem to work with these lines in... Don't know why?
+#from flask.ext.sqlalchemy import SQLAlchemy
 
 import os
 
 app = Flask(__name__)
 
 Bootstrap(app)
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
 
 app.config.from_object(os.environ.get('SETTINGS'))
