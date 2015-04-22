ffdc7fd0f7b01370b5a7bf22ddbaba3fdd201cfc
alex blewett <alex.blewett@digital.landregistry.gov.uk>
2015-04-22 14:34:03 +0100

    front end improvements

--- application/server.py
+++ application/server.py
@@ -3,39 +3,84 @@ from flask import render_template
 #, flash, redirect, request, session, make_response
 
 #simple structure to hold harded coded test data
+class User(object):
+    def __init__(self, name, line_manager, job_title, trill_role):
+        self.name = name
+        self.line_manager = line_manager
+        self.job_title = job_title
+        self.trill_role = trill_role
+        self.group_list = []
+        
+    def Add_skill_group(self, skill_group):
+        self.group_list.append(skill_group)
+        
 class Skill_group(object):
     def __init__(self, name, n):
         self.name = name
         self.n = n
+        self.title_list = []
+        
+    def Add_skill_title(self, skill_title):
+        self.title_list.append(skill_title)
+        
+class Skill_title(object):
+    def __init__(self, name):
+        self.name = name
         self.skill_list = []
 
-    def Add_skill(self, skill_name, skill_desc):
-        self.skill_list.append(skill_name +' - '+ skill_desc)
+    def Add_skill(self, skill_desc):
+        self.skill_list.append(skill_desc)
+#end of data structure
 
 #hard coded test data for prototype
-group_list = []
-
 user_name = 'Alex Blewett'
 trill_role = 'Developer'
 job_title = 'Front end web developer'
 line_manager = 'Marc McCoy'
+user = User(user_name, line_manager, job_title, trill_role)
 
-data = Skill_group('Skill Group 1 - code', 1)
-data.Add_skill('Develop code', 'produce bug free code')
-data.Add_skill('Develop code', 'unit test code')
-group_list.append(data)
+skill_group_name = 'Skill Group 1 - code'
+skill_group = Skill_group(skill_group_name, 1)
 
-data = Skill_group('Skill Group 2 - agile', 2)
-data.Add_skill('Plan work', 'work in agile')
-data.Add_skill('Plan work', 'attend stand up')
-data.Add_skill('Plan work', 'attend planning meeting')
-group_list.append(data)
+skill_title = Skill_title('Develop code')
+skill_title.Add_skill('produce bug free code')
+skill_title.Add_skill('unit test code')
+skill_group.Add_skill_title(skill_title)
 
-data = Skill_group('Skill Group 3 - deliver', 3)
-data.Add_skill('Present work', 'show team')
-data.Add_skill('Present work', 'show product owner')
-data.Add_skill('Present work', 'show n tell')
-group_list.append(data)
+skill_title = Skill_title('Design code')
+skill_title.Add_skill('Model objects')
+skill_title.Add_skill('Review design')
+skill_group.Add_skill_title(skill_title)
+
+user.Add_skill_group(skill_group)
+
+skill_group_name = 'Skill Group 2 - agile'
+skill_group = Skill_group(skill_group_name, 2)
+
+skill_title = Skill_title('Plan work')
+skill_title.Add_skill('work in agile')
+skill_title.Add_skill('attend stand up')
+skill_title.Add_skill('attend planning meeting')
+skill_group.Add_skill_title(skill_title)
+
+skill_title = Skill_title('Do scrum')
+skill_title.Add_skill('estimate story points')
+skill_title.Add_skill('feedback during retrospective')
+skill_title.Add_skill('do other srum stuff')
+skill_group.Add_skill_title(skill_title)
+
+user.Add_skill_group(skill_group)
+
+skill_group_name = 'Skill Group 3 - deliver'
+skill_group = Skill_group(skill_group_name, 3)
+
+skill_title = Skill_title('Present work')
+skill_title.Add_skill('show team')
+skill_title.Add_skill('show product owner')
+skill_title.Add_skill('show n tell')
+skill_group.Add_skill_title(skill_title)
+
+user.Add_skill_group(skill_group)
 #end of test data
 
 #possible future functionality
@@ -43,9 +88,9 @@ group_list.append(data)
 def welcome():
     return render_template('welcome.html')
 
-@app.route('/view')
+@app.route('/record')
 def view_skills():
-    return render_template('view_skills.html', user_name = user_name, trill_role = trill_role, group_list = group_list, job_title = job_title, line_manager = line_manager)
+    return render_template('view_skills.html', user_obj = user)
 
 @app.route('/edit')
 def edit_skills():
@@ -54,11 +99,12 @@ def edit_skills():
 @app.route('/admin')
 def admin_func():
     return render_template('admin_func.html')
+#end of future functionality
 
 #the prototype functionality is here
 @app.route('/')
 def test_skills():
-    return render_template('view_skills_proto.html', user_name = user_name, trill_role = trill_role, group_list = group_list)
+    return render_template('view_skills_proto.html', user_obj = user)
     return 'ok', 200
 
 
