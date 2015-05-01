8dfef664dd62f4bdf34bc9ae466d16704169c67e
SimonRAllis <simon.allis@digital.landregistry.gov.uk>
2015-05-01 10:43:00 +0100

    Password hash

--- database-setup/load/hashPWs.py
+++ database-setup/load/hashPWs.py
@@ -1,6 +1,16 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-import sys
+from werkzeug.security import generate_password_hash
+import csv
 
-with open('/home/vagrant/trill/database-setup/load/users.csv', 'r') as f:
-    for line in f:
-        print (line)
+ofile = open('usershashed.csv', 'w')
+usewriter = csv.writer(ofile,delimiter=',')
+
+with open('users.csv', 'r') as ifile:
+    userreader = csv.reader(ifile)
+    for row in userreader:
+        print(', '.join(row))
+        pwHash = generate_password_hash(row.pop())
+        row.append(pwHash)
+        usewriter.writerow(row)
+
+ifile.close()
+ofile.close()
