afd0875ebd328ca4d250765848ef55acc0cde6f2
daveogorman <david.ogorman@digital.landregistry.gov.uk>
2015-05-15 08:45:23 +0100

    add feature files for sprint 3

---
+++ acceptance-tests/features/step_definitions/sprint3.rb
@@ -0,0 +1,17 @@
+ # steps
+
+When(/^I record my IS Systems skills$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^my IS Systems skills will displayed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I record my IS skills$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^my IS skills will displayed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
