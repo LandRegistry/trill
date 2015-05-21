73b7ab5cd540d030371aa59724f13525f1a8d0a4
daveogorman <david.ogorman@digital.landregistry.gov.uk>
2015-05-21 11:10:09 +0100

    add implementation steps

--- acceptance-tests/features/step_definitions/sprint3.rb
+++ acceptance-tests/features/step_definitions/sprint3.rb
@@ -56,7 +56,7 @@ end
 
 Given(/^I select Knowledge known to be held as None$/) do
   pending # Write code here that turns the phrase above into concrete actions
-  
+
   # reuse the create_skill(mySkillgroup, myValue1, myValue2)
   #  mySkillgroup = ".//*[@id='IS-knowledge-group1']/a"
   #  myValue1 = "//*[@id='prof_radio|Understanding the Digital transformation|BDS.1.1|4']"
@@ -113,16 +113,46 @@ end
 
 Then(/^I will see a results for the selected Expert Skill$/) do
   pending # Write code here that turns the phrase above into concrete actions
+
+  #trill_logout()
+  #trill_login()
+
+  #mySkillclass = 'IS Skills'
+  #mySkillgroup = "//*[@id='IS-skill-group1']/a"
+  #myValue1 = './/input[@name="myskillname" and @checked]'
+  #check_skill(mySkillclass, mySkillgroup, myValue1)
+
 end
 
 Then(/^I will see a results for the selected Proficient Skill$/) do
   pending # Write code here that turns the phrase above into concrete actions
+
+  #trill_logout()
+  #trill_login()
+
+  #mySkillclass = 'IS Skills'
+  #mySkillgroup = "//*[@id='IS-skill-group1']/a"
+  #myValue1 = './/input[@name="myskillname" and @checked]'
+  #check_skill(mySkillclass, mySkillgroup, myValue1)
+
 end
 
 Then(/^I will see a results for the selected Basic Knowledge$/) do
   pending # Write code here that turns the phrase above into concrete actions
+
+  #trill_logout()
+  #trill_login()
+
+  #mySkillclass = 'IS Skills'
+  #mySkillgroup = "//*[@id='IS-skill-group1']/a"
+  #myValue1 = './/input[@name="myskillname" and @checked]'
+  #check_skill(mySkillclass, mySkillgroup, myValue1)
+
 end
 
 Then(/^I will see a message stating that no results were found for the search$/) do
   pending # Write code here that turns the phrase above into concrete actions
+
+  #assert page.has_content?("No results found for your search")
+
 end
