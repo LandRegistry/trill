a7abd1d2524c55083a18a70ed197e8e20314b59d
Matt Gaspar <matt.gaspar@digital.landregistry.gov.uk>
2015-05-21 11:46:14 +0100

    Add server health test for CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,16 +11,18 @@ class TestSequenceFunctions(unittest.TestCase):
         app.config.update(CONFIG_DICT)
         self.app = app.test_client()
 
+    def test_server_health(self):
+        self.assertEqual((self.app.get('/health')).data.decode("utf-8"),"OK")
+        
     def test_landing_page(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 302)
         self.assertTrue('/signin' in response.location)
-        
+
     def test_signin_page(self):
         response = self.app.get('/signin')
         self.assertEqual(response.status_code, 200)
-        
+
     def test_skill_page(self):
         response = self.app.get('/record')
         self.assertEqual(response.status_code, 302)
-
