58c8d131834bf907dbcf7e7f17a149f6f5022ffd
daveogorman <david.ogorman@digital.landregistry.gov.uk>
2015-05-22 14:10:03 +0100

    refactoring of file structure

---
+++ acceptance-tests/features/step_definitions/is_knowledge.rb
@@ -0,0 +1,21 @@
+Given(/^I select Knowledge known to be held as Basic$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+
+  # reuse the create_skill(mySkillgroup, myValue1, myValue2)
+  #  mySkillgroup = ".//*[@id='IS-knowledge-group1']/a"
+  #  myValue1 = "//*[@id='prof_radio|Understanding the Digital transformation|BDS.1.1|1']"
+  #  myValue2 = "//*[@id='prof_radio|Understanding the Digital transformation|BDS.1.1|2']"
+  #  create_skill(mySkillgroup, myValue1, myValue2)
+
+end
+
+Given(/^I select Knowledge known to be held as None$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+
+  # reuse the create_skill(mySkillgroup, myValue1, myValue2)
+  #  mySkillgroup = ".//*[@id='IS-knowledge-group1']/a"
+  #  myValue1 = "//*[@id='prof_radio|Understanding the Digital transformation|BDS.1.1|4']"
+  #  myValue2 = "//*[@id='prof_radio|Understanding the Digital transformation|BDS.1.1|1']"
+  #  create_skill(mySkillgroup, myValue1, myValue2)
+
+end
