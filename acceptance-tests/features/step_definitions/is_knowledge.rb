0ebb5b5baa20064f725e725b69a9c6e231859352
daveogorman <david.ogorman@digital.landregistry.gov.uk>
2015-06-12 08:56:19 +0100

    fix for us187

--- acceptance-tests/features/step_definitions/is_knowledge.rb
+++ acceptance-tests/features/step_definitions/is_knowledge.rb
@@ -20,8 +20,8 @@ end
 When(/^I record my IS Systems skills$/) do
   #pending # Write code here that turns the phrase above into concrete actions
   mySkillgroup = ".//*[@id='IS-skill-group4']/a"
-  myValue1 = "//*[@id='prof_radio||TS.1.1|1']"
-  myValue2 = "//*[@id='prof_radio|9493929190898887dsfgfbvcfghr|TS.1.1|2']"
+  myValue1 = "//*[@id='prof_radio|121|1']"
+  myValue2 = "//*[@id='prof_radio|121|2']" #//*[@id="prof_radio|121|1"]
   create_skill(mySkillgroup, myValue1, myValue2)
 
 end
@@ -31,7 +31,10 @@ Then(/^my IS Systems skills will displayed$/) do
   #pending # Write code here that turns the phrase above into concrete actions
   mySkillclass = 'IS Skills'
   mySkillgroup = "//*[@id='IS-skill-group4']/a"
-  myValue1 = './/input[@name="prof_radio_9493929190898887dsfgfbvcfghr_TS.1.1" and @checked]'
+
+  myValue1 = './/input[@name="prof_radio_121" and @checked]'
+
+  #myValue1 = './/input[@name="prof_radio_9493929190898887dsfgfbvcfghr_TS.1.1" and @checked]'
   check_skill(mySkillclass, mySkillgroup, myValue1)
 
 end
